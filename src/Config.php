4c6a3cf557d9a248ed2c9040b9996fb3511b57e8
Andrei Baranov <andrei.baranov@virta.global>
2020-04-24 12:37:52 +0300

    Personalise config

--- src/Config.php
+++ src/Config.php
@@ -11,6 +11,6 @@ final class Config
     public const SUPERMETRICS_POSTS_ENTRY_POINT = self::SUPERMETRIC_BASE_URL . '/assignment/posts';
 
     public const SUPERMETRICS_CLIENT_ID = 'ju16a6m81mhid5ue1z3v2g0uh';
-    public const SUPERMETRICS_CLIENT_EMAIL = 'your@email.address';
-    public const SUPERMETRICS_CLIENT_NAME = 'Your Name';
+    public const SUPERMETRICS_CLIENT_EMAIL = 'justone1024@gmail.com';
+    public const SUPERMETRICS_CLIENT_NAME = 'Andrei Baranov';
 }
